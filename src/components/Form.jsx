d408f2cf938d2892573c73882f61b43b09dda1d8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-18 16:56:39 +0200

    added delete post title

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,51 @@
+import { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Form = () => {
+
+    const [posts, setPosts] = useState([]);
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        // Se il titolo ha almeno un carettere e se non è gia presente nei posts allora pushalo
+        if (title.trim().length !== 0 && !posts.includes(title.trim())) setPosts(array => ([...array, title]));
+
+        setTitle('');
+    }
+
+    const removePost = (index) => {
+        setPosts(array => array.filter((post, i) => i !== index));
+    }
+
     return (
         <>
             <h1>Inserisci il titolo del tuo Post</h1>
-            <form>
-                <label> Inserisci il titolo</label>
-                <input type="text" />
-                <button>Salva</button>
-            </form>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <label> Inserisci il titolo</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <button>Salva</button>
+                </form>
+                <ul>
+                    {posts.map((post, index) => (
+                        <li key={`post-${index}`}>
+                            {post}
+                            <button
+                                onClick={() => { removePost(index) }}
+                            >
+                                <IoCloseSharp /></button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
+
     )
 }
 
